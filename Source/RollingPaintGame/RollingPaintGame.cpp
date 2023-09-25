25d54db079fbef17a02a675b467099226b254aac
Ivan Seleznov <ivan14.02434@gmail.com>
2023-09-25 23:06:08 +0300

    Initial commit

---
+++ Source/RollingPaintGame/RollingPaintGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RollingPaintGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RollingPaintGame, "RollingPaintGame" );
---
+++ Source/RollingPaintGame/RollingPaintGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RollingPaintGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROLLINGPAINTGAME_API ARollingPaintGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
